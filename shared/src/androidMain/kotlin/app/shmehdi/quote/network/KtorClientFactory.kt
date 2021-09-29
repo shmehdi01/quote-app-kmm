71245201580cccc25f47e41ff4f195f358ce0a9a
Syed Hussain Mehdi <shmehdi01@gmail.com>
2021-09-29 12:55:09 +0530

    ADDED: Install Header for Content Type

--- shared/src/androidMain/kotlin/app/shmehdi/quote/network/KtorClientFactory.kt
+++ shared/src/androidMain/kotlin/app/shmehdi/quote/network/KtorClientFactory.kt
@@ -2,12 +2,20 @@ package app.shmehdi.quote.network
 
 import io.ktor.client.*
 import io.ktor.client.engine.android.*
+import io.ktor.client.features.*
 import io.ktor.client.features.json.*
 import io.ktor.client.features.json.serializer.*
+import io.ktor.client.request.*
+import io.ktor.http.*
 
 actual class KtorClientFactory {
 
     actual fun create() : HttpClient = HttpClient(Android) {
+
+        install(DefaultRequest) {
+            header(HttpHeaders.ContentType, ContentType.Application.Json)
+        }
+
         install(JsonFeature) {
             serializer = KotlinxSerializer(
                 json = kotlinx.serialization.json.Json {
