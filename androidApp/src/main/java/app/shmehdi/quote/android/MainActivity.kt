5d92e48a2e925ae51c091323237df6dda978866d
Syed Hussain Mehdi <shmehdi01@gmail.com>
2021-10-01 12:05:43 +0530

    REFACTOR: code clean up

--- androidApp/src/main/java/app/shmehdi/quote/android/MainActivity.kt
+++ androidApp/src/main/java/app/shmehdi/quote/android/MainActivity.kt
@@ -17,25 +17,11 @@ fun greet(): String {
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
-    val viewModel: AuthViewModel by viewModels()
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         val tv: TextView = findViewById(R.id.text_view)
         tv.text = greet()
-
-        viewModel.login(LoginRequest("mehdi@gmail.com", "password"))
-
-        viewModel.isLoading.observe(this) {
-            Log.d("TAG","Loading $it")
-            tv.text = "Loading $it"
-        }
-
-        viewModel.error.observe(this) {
-            Log.d("TAG","Error ${it.errorMessage} code ${it.errorCode}")
-            tv.text = it.errorMessage
-        }
     }
 }
