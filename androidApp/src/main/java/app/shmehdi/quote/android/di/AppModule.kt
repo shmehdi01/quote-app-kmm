3d55cdf750fbf68072890e088bba5aa56a67cef2
Syed Hussain Mehdi <shmehdi01@gmail.com>
2021-10-01 10:23:29 +0530

    REFACTOR: sharedpref injection moved to App module

--- androidApp/src/main/java/app/shmehdi/quote/android/di/AppModule.kt
+++ androidApp/src/main/java/app/shmehdi/quote/android/di/AppModule.kt
@@ -1,19 +1,17 @@
 package app.shmehdi.quote.android.di
 
 import android.app.Application
-import app.shmehdi.quote.repository.AuthRepository
 import app.shmehdi.quote.utils.preference.AppPreference
 import app.shmehdi.quote.utils.preference.AppPreferenceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
-import dagger.hilt.android.components.ViewModelComponent
+import dagger.hilt.components.SingletonComponent
 
-@InstallIn(ViewModelComponent::class)
+@InstallIn(SingletonComponent::class)
 @Module
-class RepositoryModule {
+class AppModule {
 
     @Provides
-    fun provideAuthRepository() = AuthRepository()
-
+    fun provideSharePref(application: Application) : AppPreference = AppPreferenceImpl(application)
 }
