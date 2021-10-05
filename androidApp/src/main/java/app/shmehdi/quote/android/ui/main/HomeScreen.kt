cb41ee3ddb4830f5034f028e7ab421f1773fe67f
Syed Hussain Mehdi <shmehdi01@gmail.com>
2021-10-05 22:23:17 +0530

    ADDED: Home screen

---
+++ androidApp/src/main/java/app/shmehdi/quote/android/ui/main/HomeScreen.kt
@@ -0,0 +1,9 @@
+package app.shmehdi.quote.android.ui.main
+
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun HomeScreen() {
+    Text(text = "Home Screen")
+}
