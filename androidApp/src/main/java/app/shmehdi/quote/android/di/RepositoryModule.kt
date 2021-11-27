cd79fe1adea17523543bb67c39bb785948a05316
Syed Hussain Mehdi <shmehdi01@gmail.com>
2021-11-27 19:44:33 +0530

    REFACTOR: remvoed unused imports

--- androidApp/src/main/java/app/shmehdi/quote/android/di/RepositoryModule.kt
+++ androidApp/src/main/java/app/shmehdi/quote/android/di/RepositoryModule.kt
@@ -1,9 +1,6 @@
 package app.shmehdi.quote.android.di
 
-import android.app.Application
 import app.shmehdi.quote.repository.AuthRepository
-import app.shmehdi.quote.utils.preference.AppPreference
-import app.shmehdi.quote.utils.preference.AppPreferenceImpl
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
